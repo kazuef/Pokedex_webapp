ac04e632dcc2d09a226c0b959ab6f1b8673be0d9
kazair <tokubd127@gmail.com>
2024-12-01 13:52:52 +0900

    全てのIPを受け入れるよう修正

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: '0.0.0.0', // すべてのIPアドレスで受け入れる
+    port: 5173,       // ポート設定（必要に応じて変更）
+  },
 });
